809fe9517b059629e92e73d8b967676b34af6fce
Maxim Kabanov <kabanov.maks.frontend@gmail.com>
2023-09-25 20:21:56 +0300

    Settings project and base architecture

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  "extends": [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', '@typescript-eslint', 'jest', 'import'],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: ['./tsconfig.json',],
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'linebreak-style': 'off',
+    'react/function-component-definition': 'off',
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: [
+        'state',
+      ]
+    }],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
---
+++ src/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import style from './App.module.scss'
+
+const App: React.FC = () => {
+	return (
+		<div className={style.wrapper}>
+			<h1>App</h1>
+		</div>
+	)
+}
+
+export default App
